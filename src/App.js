49c900c3f4c8c726385f166640eb220994a60cef
Elco Garcia <garciaelco18@gmail.com>
2019-09-30 14:34:48 -0500

    can create todos now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		todos: [ 'First Item', 'Second Item' ],
+		inputValue: ''
+	};
+	//the only thing this should handle  is actually the todo state
+	handleCreateTodo = (todo) => {
+		this.setState({
+			todos: [ todo, ...this.state.todos ]
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.handleCreateTodo(this.state.inputValue);
+		this.setState({
+			inputValue: ''
+		});
+		//call our state updater here
+	};
+
+	handleOnChange = (event) => {
+		this.setState({
+			inputValue: event.target.value
+		});
+	};
+	render() {
+		const renderTodos = this.state.todos.map((item, index) => {
+			return (
+				<li key={index} className="todo">
+					{item}
+				</li>
+			);
+		});
+		return (
+			<div className="App" onSubmit={this.handleSubmit}>
+				<ul className="todos">{renderTodos}</ul>
+				<form className="add-form">
+					<input type="text" onChange={this.handleOnChange} value={this.state.inputValue} />
+					<input type="submit" value="Add" />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
